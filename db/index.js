486982b8943179a8fe23a6a910d2a22eec653120
James Jewitt <jamestjewitt@gmail.com>
2022-08-04 12:07:57 -0400

    Include .gitignore

---
+++ db/index.js
@@ -0,0 +1,13 @@
+/*  establish connection to equityDatabase */
+
+const mongoose=require('mongoose')
+let MONGODB_URI='mongodb://127.0.0.1:27017/equityDatabase'
+
+mongoose 
+    .connect(MONGODB_URI)
+    .then(()=>{console.log('Successfully connected to equityDatabase in MongoDB.')})
+    .catch(e=>{console.log('Connection error...',e.message)})
+
+const db=mongoose.connection
+
+module.exports=db
