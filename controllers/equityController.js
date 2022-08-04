d0afff6db98ff6f44158386526a652702f856884
James Jewitt <jamestjewitt@gmail.com>
2022-08-04 15:23:38 -0400

    Successfully retrieve market-data via Controller

--- controllers/equityController.js
+++ controllers/equityController.js
@@ -3,13 +3,13 @@ const Equity=require('../models/equity')
 
 const getData=async (req,res)=>{
     try {
-        yF.historical({
+        await yF.historical({
             symbol:'AAPL',
             from:'2021-01-01',
             to:'2021-07-01',
             period:'d'
         },function(err,quotes){
-            return quotes
+            res.send(quotes)
         })
 
     } catch(error){
