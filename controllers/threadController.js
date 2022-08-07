06d0a8bea6993cc8b0e84ae1beaf990792a1db99
James Jewitt <jamestjewitt@gmail.com>
2022-08-06 23:37:26 -0400

    deleteThread and last push fix many-to-many in thread

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -3,18 +3,21 @@ const Thread=require('../models/thread')
 const Equity=require('../models/equity')
 const Account=require('../models/account')
 
-
-
 const createThread=async (req,res)=>{
     const {equityId,accountId,title}=req.body
     const equity=await Equity.findById(equityId)
     const account=await Account.findById(accountId)
-    const newThread=await Thread.create({equity:equityId,creator:accountId,title})
+    const newThread=await (await Thread.create({equity:equityId,creator:accountId,title}))
     equity.threads.push(newThread)
     equity.save()
     account.threads.push(newThread)
     account.save()
     res.status(200).json(newThread,equity,account)
+}
+const deleteThread=async (req,res)=>{
+    const {id}=req.params
+    const deletedThread=await Thread.findByIdAndDelete(id)
+    res.status(200).json(deletedThread)
 }
 
 
